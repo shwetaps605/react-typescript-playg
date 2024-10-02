43a66988a8b05bb44be4783e014e63b24d9a0df0
SHWEROY_T3DT <shweroy@apac.corpdir.net>
2024-10-02 17:53:21 +0530

    learned some hooks

--- src/events/GuestList.tsx
+++ src/events/GuestList.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useRef, useState } from "react";
+import { wait } from "@testing-library/user-event/dist/utils";
+import React, { useContext, useEffect, useMemo, useRef, useState } from "react";
+import { ThemeContext } from "../context/useTheme";
+import { signal } from "@preact/signals-react";
 
 interface GuestListProps {
     users: string[] | undefined,
@@ -6,22 +9,47 @@ interface GuestListProps {
 }
 
 const GuestList: React.FC<GuestListProps> = ({users,onNewGuest}) => {
+    // const { darkTheme } = useContext(ThemeContext);
+    // console.log("THEME CONTEXT->", darkTheme)
     const [name,setName] = useState('');
     // const [guests,setGuests] = useState<string[]>([])
     const inputRef = useRef<HTMLInputElement|null>(null);
+    const [waitList, setWaitList] = useState(0);
+
+    const superSlowFunction = () => {
+        for(let i=0;i<1000000000;i++);
+        return name + " ick"
+    }
+
+    const item = signal("ROY")
+    console.log("item->", item)
+
+    // const newName = useMemo(()=>{
+    //     return superSlowFunction()
+    // },[name])
 
     useEffect(()=>{
         // if(!users)return
         // setGuests(users)
-
+        console.log("USE EFECT called")
+        console.log(inputRef.current?.value)
         if(!inputRef.current) return
         inputRef.current.focus();
+        setWaitList(prev => users ? 6 - users?.length : prev)
     },[])
 
+    useEffect(()=>{
+        console.log("USE EFFECT called for users")
+        setWaitList(prev => users ? 6 - users?.length : prev)
+    },[users])
+
     const onAddGuestClick = () => {
-        setName("");
         onNewGuest(name);
-        //setGuests([...guests,name])
+        if(inputRef.current) {
+            inputRef.current.value = "";
+            if(waitList !== 0)
+            inputRef.current.focus();
+        }
     }
 
     const onInputNameChange = (e:React.ChangeEvent<HTMLInputElement>) => {
@@ -29,8 +57,14 @@ const GuestList: React.FC<GuestListProps> = ({users,onNewGuest}) => {
     }
 
     const onDragStart =(event:React.DragEvent<HTMLDivElement>)=>{
-        console.log(event)
+        //console.log(event)
     }
+
+    const isWaitlistFull = () => {
+        return waitList === 0 ? true : false;
+    }
+
+    
 
     return(
         <section>
@@ -38,11 +72,16 @@ const GuestList: React.FC<GuestListProps> = ({users,onNewGuest}) => {
             Guest List component!
             </h3>
            
-            <input ref={inputRef} type="text" value={name} onChange={onInputNameChange} />
+            <input ref={inputRef} type="text" onChange={onInputNameChange} disabled={isWaitlistFull()} />
             <button onClick={onAddGuestClick}>Add Guest</button>
-            <div draggable onDragStart={onDragStart}>
+            {/* <div draggable onDragStart={onDragStart}>
                 Drag ME
+            </div> */}
+            
+            <div>
+                { waitList === 0 ? 'WAITLIST FULL' : 'current space in waitlist->' +waitList }
             </div>
+
         </section>
     )
 }
